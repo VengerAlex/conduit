d6c9ab79b2683626f171cf6f142ea26475a10b8a
VengerAlex <alexvenger90@gmail.com>
2022-11-14 14:17:52 +0200

    ADD: husky & prettier & eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 export const App = () => {
-    return <>
-        <h1 className="font-bold text-blue-300">Hello World</h1>
-    </>
-}
+  return (
+    <h1 className="font-bold text-blue-300 text-blue-300 text-blue-300 text-blue-300 text-blue-300 text-blue-300 text-blue-300 text-blue-300 text-blue-300">
+      Hello World
+    </h1>
+  );
+};
